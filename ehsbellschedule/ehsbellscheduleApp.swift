fe55b4728bade519e53e5b22e7c304b0d8cb0336
alex l <alexliao365@gmail.com>
2025-08-10 21:13:39 -0700

    Initial Commit

---
+++ ehsbellschedule/ehsbellscheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ehsbellscheduleApp.swift
+//  ehsbellschedule
+//
+//  Created by Alex Liao on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ehsbellscheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ehsbellscheduleTests/ehsbellscheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ehsbellscheduleTests.swift
+//  ehsbellscheduleTests
+//
+//  Created by Alex Liao on 8/10/25.
+//
+
+import Testing
+@testable import ehsbellschedule
+
+struct ehsbellscheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
